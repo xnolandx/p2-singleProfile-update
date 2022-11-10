d12633e5c653fdaa9b158cc9b9faffadc1894c4f
Jeff <mynameisnoland@gmail.com>
2022-11-10 11:24:19 -0500

    update2

--- src/components/SingleEvent.js
+++ src/components/SingleEvent.js
@@ -45,30 +45,40 @@ useEffect(() => {
     }})
 
   return (
-    <div className='event-container'>
 
-      <h1 className='title'>{event.event.title}</h1>
-      <div className='single-event-image'>
+
+    <>
+    
+      <h1 className='title'>"{event.event.title}"</h1>
+      <h2 className='event-id-num'>Event Identification Number: {event.event.id} </h2>
+    
+    
+    <div className='event-container'>
+      
+ 
+      
+      <div classname='flex-flex-flex'>
+      <div className='event-description'>
+          <b><u><h4 className='description-tag'>Description: </h4></u></b><br></br>
+          <span>{event.event.description}</span>
+        </div>
+
+
+        <div className='event-timeline'>
+          <b><u><h4 className='event-timeline-tag'>Event Timeline:</h4></u></b><br></br>
+          <span>Start: {event.event.start}</span>
+          <span>End: {event.event.end}</span>
+        </div>
+
+        <div className='single-event-image'>
             <img className='single-event-image' src={event.event.thumbnail.path +'.'+ event.event.thumbnail.extension} alt=''/>
       </div>
-      <h2 className='event-id-num'>Event Identification Number: {event.event.id} </h2>
+      
 
-      <ul className='event-description'>
-        Description:
-        <li> {event.event.description}</li>
-      </ul>
-
-      <div className='event-info-container'>
-
-        <ul className='event-timeline'>
-          Event Timeline:
-          <li>Start: {event.event.start}</li>
-          <li>End: {event.event.end}</li>
-        </ul>
-
-          Assosciated Characters:
-          <div className="event-characters-container">
+      <div className="event-characters-container">
+          {/* <b><u><h4 className='event-timeline-tag'>Assosciated Characters:</h4></u></b><br></br> */}
             <div className="event-character-list">{
+
                 (matchingResults.length === 0 ? <div>Loading... </div> : 
                     matchingResults.map((character, index) => (
                         <Link to={`/characters/${character.results[0].id}`} key={character.results[0].id} onClick={() => {
@@ -81,12 +91,50 @@ useEffect(() => {
                     )))
             }</div>
         </div>
-          
+
         
 
 
+
+
+        {/* <div className="event-characters-container"> 
+            <div className="event-character-list">{
+                (eventURL === '' || eventURL === undefined || eventURL.length === 0  ?  <div>No known event data.</div> : 
+                    eventData.map((event, index) => (
+                        <Link to={`/events/${event.id}`} key={event.id} onClick={() => {
+                            setEventCon(event)
+                        }} className='event-link-text'>
+                            
+                            <div className='single-event-card' key={event.id}>
+                                <h5 className="thumbnail-caption">{event.title}</h5>
+                                <img src={event.thumbnail.path +'.'+ event.thumbnail.extension} alt='' className='events-thumbnail'/>
+                                <div className="event-text">
+                                
+                                </div>
+                            </div>
+                        </Link>
+                    )))
+            }
+            </div>
+        </div> */}
+
+
       </div>
+
+
+
+
+
+
+        
+          
+
+
+      
+
+
     </div>
+    </>
   )
 
 }
